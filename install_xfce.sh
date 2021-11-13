948f2493e99ec674077cf5125edfe923693bcefa
Stephen Steward <smsteward92@gmail.com>
2021-11-13 15:25:41 -0500

    Add UFW firewall install and base system comment

--- install_xfce.sh
+++ install_xfce.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# It is assumed that you have already partitioned your disks how you want
-# If you haven't done this already, go back and do it now before continuing
+# It is assumed that you have already partitioned your disks and used 'pacstrap' to install the base system
+# If you haven't done this already, go back and do it now before continuing on with this script
+# An example base system install could be: 'base linux base-devel linux-firmware amd-ucode git vim'
 
 # Ensure system clock is accurate
 sudo timedatectl set-ntp true
@@ -16,9 +17,11 @@ makepkg -si --noconfirm
 
 # Install the XFCE desktop environment with lightdm display manager
 sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies
-
-# Enable lightdm display manager
 sudo systemctl enable lightdm
+
+# Install ufw firewall and gui app
+sudo pacman -S --noconfirm ufw gufw
+sudo systemctl enable ufw
 
 # Reboot the machine
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
